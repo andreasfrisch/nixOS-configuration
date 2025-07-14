344b79446016ce1b61d52fc61e71cde95414b0ac
Andreas Frisch <andreas.frisch@gmail.com>
2025-07-14 11:09:35 +0200

    add direnv

--- home.nix
+++ home.nix
@@ -72,6 +72,14 @@
     };
   };
 
+  programs = {
+    direnv = {
+      enable = true;
+      enableZshIntegration = true; # see note on other shells below
+      nix-direnv.enable = true;
+    };
+  };
+
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
