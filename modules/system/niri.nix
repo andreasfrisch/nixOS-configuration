679f9e2cdda595ad3a23b88f32ed74d492ebc9f3
Andreas Frisch <andreas.frisch@gmail.com>
2026-08-21 15:22:39 +0200

    better niri part 1

--- modules/system/niri.nix
+++ modules/system/niri.nix
@@ -2,6 +2,18 @@
 
 {
   config = lib.mkIf (userSettings.wm == "niri") {
-    programs.niri.enable = true;
+    programs.niri = {
+      enable = true;
+      settings = {
+        prefer-no-csd = true;
+        workspaces = [ "1" "2" "3" "4" "5" "6" "7" "8" "9" ];
+        layout = {
+          gaps = 10;
+          focus-ring = {
+            width = 2;
+          };
+        };
+      };
+    };
   };
 }
